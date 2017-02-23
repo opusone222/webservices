5f3e412ba7e8a05bacb192ec5ad37ed44599cc7c
vytlasai <opusone222@gmail.com>
2017-02-23 17:47:19 -0500

    Hello

--- Product-Services/src/main/java/com/sidgs/com/sidgs/service/ProductServiceImpl.java
+++ Product-Services/src/main/java/com/sidgs/com/sidgs/service/ProductServiceImpl.java
@@ -1,35 +1,51 @@
 package com.sidgs.com.sidgs.service;
 
+import com.sidgs.dao.ProductDao;
 import com.sidgs.model.Product;
+import org.springframework.beans.factory.annotation.Autowired;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 /**
  * Created by Vytlasai on 2/23/2017.
  */
 public class ProductServiceImpl implements ProductService {
+
+
+    @Autowired
+    private ProductDao productDao;
+
     @Override
+    @Transactional
     public void addProduct(Product product) {
-
+        productDao.addProduct(product);
     }
 
     @Override
+    @Transactional
     public List<Product> getAllProducts() {
-        return null;
+        return productDao.getAllProducts();
+
     }
 
     @Override
+    @Transactional
     public void deleteProduct(Integer productId) {
-
+        productDao.deleteProduct(productId);
     }
 
     @Override
     public Product getProduct(int productId) {
-        return null;
+        return productDao.getProduct(productId);
     }
 
     @Override
     public Product updateEmployee(Product product) {
-        return null;
+        return productDao.updateProduct(product);
+    }
+
+    public void setProductDao(ProductDao productDao){
+        this.productDao = productDao;
     }
 }
