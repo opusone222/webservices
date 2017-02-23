3d67228994aa84493c0ffc5907aae500924a6a1b
vytlasai <opusone222@gmail.com>
2017-02-23 17:11:14 -0500

    Please follow this template

---
+++ Product-Services/src/main/java/com/sidgs/com/sidgs/service/ProductServiceImpl.java
@@ -0,0 +1,35 @@
+package com.sidgs.com.sidgs.service;
+
+import com.sidgs.model.Product;
+
+import java.util.List;
+
+/**
+ * Created by Vytlasai on 2/23/2017.
+ */
+public class ProductServiceImpl implements ProductService {
+    @Override
+    public void addProduct(Product product) {
+
+    }
+
+    @Override
+    public List<Product> getAllProducts() {
+        return null;
+    }
+
+    @Override
+    public void deleteProduct(Integer productId) {
+
+    }
+
+    @Override
+    public Product getProduct(int productId) {
+        return null;
+    }
+
+    @Override
+    public Product updateEmployee(Product product) {
+        return null;
+    }
+}
